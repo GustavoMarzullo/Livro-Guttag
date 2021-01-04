bc62a0a60aaa3533ed750a702f37a7910b41b2da
Gustavo Marzullo <gustavmarzullo@gmail.com>
2021-01-04 13:03:59 -0300

    .

--- Derivada.py
+++ Derivada.py
@@ -1,13 +1,15 @@
-import numpy as np
-def dif(x,h=1e-5):
+
+def dif(f,x,h=1e-5):
     try:
         derivada=(-f(x+2*h)+8*f(x+h)-8*f(x-h)+f(x-2*h))/(12*h)
         return derivada
     except NameError:
         print('A função f não foi definida.')
-def dif2(x,h=1e-5):
+def dif2(f,x,h=1e-5):
     try:
         derivada=(-f(x+2*h)+16*f(x+h)-30*f(x)+16*f(x-h)-f(x-2*h))/(12*h**2)
         return derivada
     except NameError:
         print('A função f não foi definida.')
+
+print(dif(lambda x: x**2,2))
